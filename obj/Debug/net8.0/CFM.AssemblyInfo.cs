1eb59daff14909bebd29c3bb7eed0abc3863fe54
CJZZX <windgrin56@gmail.com>
2024-07-21 13:12:56 +0800

    MoreTerraWeapons Added

--- obj/Debug/net8.0/CFM.AssemblyInfo.cs
+++ obj/Debug/net8.0/CFM.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CFM")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eac4d90bf802a17c3372469938679d98871f36af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+923fc6d2cea80e9d0f2fe3cd7396126b0fb3b638")]
 [assembly: System.Reflection.AssemblyProductAttribute("CFM")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CFM")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
